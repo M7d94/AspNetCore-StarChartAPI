95d9c1c5805a5d28860b766a15a5f81169ee42bb
M7d94 <mohammed.salman.q3@gmail.com>
2020-10-02 19:42:33 +0300

    Update Startup.cs

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -10,13 +10,16 @@ namespace StarChart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            
+            Service.AddDbContext<ApplicationDbContext>{
+                ptions => options.UseInMemoryDatabase("StarChart")
+            }
+            app.UseMvc();
         }
     }
 }
